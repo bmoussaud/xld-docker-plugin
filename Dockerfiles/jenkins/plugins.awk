d00626c11b5f4d7470d2284ee23e3d59f2909e08
Benoit Moussaud <bmoussaud@xebialabs.com>
2015-11-06 16:43:53 +0100

    cleanup

--- Dockerfiles/jenkins/plugins.awk
+++
@@ -1,4 +0,0 @@
-BEGIN { FS="," }
-{
-    print "curl -sSL --create-dirs -o /usr/share/jenkins/plugins/" $1 ".hpi https://updates.jenkins-ci.org/" ($2 == "latest" ? "latest/" : "download/plugins/" $1 "/" $2 "/") $1 ".hpi"
-}
--- PetDocker/package.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-$(docker-machine env dev)
-VERSION=$1
-echo "Buid Version $VERSION"
-docker build -t petdocker/petdocker-app:$VERSION petclinic
-docker build -t petdocker/petdocker-backend-app:2.0 petclinic-backend
-sed -e "s/##VERSION##/$VERSION/g"   ./template/deployit-manifest.xml > ./dar/deployit-manifest.xml
-jar cvf PetDocker-$VERSION.dar -C dar .
